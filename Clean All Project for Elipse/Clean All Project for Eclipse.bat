132264cbca301824f240e760fbece6470a101d34
GabeGu <guguibin1988@gmail.com>
2019-04-04 11:20:03 +0800

    add "Clean All Project" batchfile
    the "Clean All Project"  is assuming that your all projects directories are in the same path.

--- Clean All Project for Elipse/Clean All Project for Eclipse.bat
+++ Clean All Project for Elipse/Clean All Project for Eclipse.bat
@@ -1,6 +1,5 @@
 ﻿@echo off
 
-echo Make Clean.bat
 if "%1" == "" (
     echo 没有指定路径，请在Eclipse的"External Tool"中传入"${project_loc}"参数
     exit
@@ -10,10 +9,9 @@ if "%1" == "" (
 
 echo Deleting...
 
-del /S %1\*.o
-del /S %1\*.d
+del /S %1\..\*.o
+del /S %1\..\*.d
 
-echo %1
-echo clean project completed %TIME%
+echo clean all projects completed %TIME%
 
-echo created by GabeGu 2019.4.4
+echo created by GabeGu 2019-4-4
